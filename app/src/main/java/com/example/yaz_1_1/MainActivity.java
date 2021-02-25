076aa669034c988d0212b7e9454b7e78df09673a
halanch599 <altavista5.99>
2021-02-25 11:26:31 +0300

    Lecture # 1-2 Design a login screen adding drawable resouce using edit text, textview and properties calling a method from button click

--- app/src/main/java/com/example/yaz_1_1/MainActivity.java
+++ app/src/main/java/com/example/yaz_1_1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.yaz_1_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Login(View view)
+    {
+
+        EditText etuserName = (EditText) findViewById(R.id.etUserName);
+        EditText etpassword = (EditText) findViewById(R.id.etPassword);
+
+        String username = etuserName.getText().toString().trim();
+        String password = etpassword.getText().toString().trim();
+
+        if (username.equals("")){
+
+        }
+
     }
 }
